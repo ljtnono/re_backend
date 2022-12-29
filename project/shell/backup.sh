4538c20faa23172e5d1bb570bd18e5cc1d741158
lingjiatong <935188400@qq.com>
2022-12-29 11:47:05 +0800

    [feat]数据库备份加上数据库结构备份

--- project/shell/backup.sh
+++ project/shell/backup.sh
@@ -19,16 +19,21 @@ backup() {
   if [ -e $SAVE_FOLDER_PATH/$DATE ]; then
     rm -rf $SAVE_FOLDER_PATH/$DATE
   fi
-  mkdir -p $SAVE_FOLDER_PATH/$DATE
+  # 只包含结构
+  mkdir -p $SAVE_FOLDER_PATH/$DATE/struct
+  # 包含数据和结构
+  mkdir -p $SAVE_FOLDER_PATH/$DATE/data
   # 备份数据库
   for database in ${DATABASES[@]}; do
-    $MYSQLDUMP_EXE_PATH -h $HOST -P $PORT -u$USER -p$PASSWORD $database > $SAVE_FOLDER_PATH/$DATE/$database.sql
+    $MYSQLDUMP_EXE_PATH -h $HOST -P $PORT -u$USER -p$PASSWORD -d $database > $SAVE_FOLDER_PATH/$DATE/struct/$database.sql
+    $MYSQLDUMP_EXE_PATH -h $HOST -P $PORT -u$USER -p$PASSWORD $database > $SAVE_FOLDER_PATH/$DATE/data/$database.sql
   done
   # 打包成为压缩包
-  cd $SAVE_FOLDER_PATH
-  tar czvf $DATE.tar.gz $DATE/*.sql
-  # 删除原文件夹
-  rm -rf $SAVE_FOLDER_PATH/$DATE
+  cd $SAVE_FOLDER_PATH/$DATE
+  tar czvf struct.tar.gz struct/*.sql
+  tar czvf data.tar.gz data/*.sql
+  rm -rf struct
+  rm -rf data
 }
 
 backup
