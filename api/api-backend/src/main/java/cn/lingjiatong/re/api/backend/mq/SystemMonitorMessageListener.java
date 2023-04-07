a7c54d844df53f96396e96d8c2fec2e8d196334f
lingjiatong <ljtnono@163.com>
2023-04-07 21:24:14 +0800

    [feat]系统监控mq消息改为广播模式

--- api/api-backend/src/main/java/cn/lingjiatong/re/api/backend/mq/SystemMonitorMessageListener.java
+++ api/api-backend/src/main/java/cn/lingjiatong/re/api/backend/mq/SystemMonitorMessageListener.java
@@ -5,6 +5,8 @@ import cn.lingjiatong.re.api.backend.websocket.WebSocketConfig;
 import cn.lingjiatong.re.api.backend.websocket.WebSocketService;
 import cn.lingjiatong.re.common.util.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.ConsumeMode;
+import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
@@ -25,7 +27,9 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 @RocketMQMessageListener(
         topic = "systemMonitor",
-        consumerGroup = "api-backend"
+        consumerGroup = "api-backend",
+        consumeMode = ConsumeMode.CONCURRENTLY,
+        messageModel = MessageModel.BROADCASTING
 )
 public class SystemMonitorMessageListener implements RocketMQListener<String> {
 
